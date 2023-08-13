e4f5f6053c1c2abc4ac6bdfc43672a2467b2a9f2
Shane A. Healy <ShaneAHealy55@gmail.com>
2023-08-12 22:06:42 -0500

    Might not be finished, might need to iterate again, just a test

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,65 @@
 
 
+const Header = (title, date, temp) => {
+  // TASK 1
+  // ---------------------
+  // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
+  // The html tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
+  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
+  //
+  //  <div class="header">
+  //    <span class="date">{ date }</span>
+  //    <h1>{ title }</h1>
+  //    <span class="temp">{ temp }</span>
+  //  </div>
+  const header = document.createElement('div');
+  const hDate = document.createElement('span');
+  const hTitle = document.createElement('h1');
+  const hTemp = document.createElement('span');
+
+  header.classList.add('header');
+  hDate.classList.add('date');
+  hTemp.classList.add('temp');
+
+  header.appendChild(hDate);
+  header.appendChild(hTitle);
+  header.appendChild(hTemp);
+
+  hDate.textContent = date;
+  hTitle.textContent = title;
+  hTemp.textContent = temp;
+
+  return header;
+}
+
+const headerAppender = (selector) => {
+  // TASK 2
+  // ---------------------
+  // Implement this function taking a css selector as its only argument.
+  // It should create a header using the Header component above, passing arguments of your choosing.
+  // It should append the header to the element in the DOM that matches the given selector.
+  //
+  const headerContainer = document.querySelector(selector);
+  headerContainer.appendChild(Header('AJ News Paper', 'Feb 24th 2023', 'City 71' + "\u00B0"));
+  // HINT: querySelector can take in a string (ie querySelector("#wrapper")) 
+  // but it can also take in a variable (ie querySelector(selector))
+  // We are taking care of passing in the correct selector on line 16,
+  // so all that you need to do is pass it into the querySelector method
+  // for the tests to work!
+}
+
+export { Header, headerAppender }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,73 @@
+import axios from "axios";
+
+const Tabs = (topics) => {
+  // TASK 3
+  // ---------------------
+  // Implement this function which takes an array of strings ("topics") as its only argument.
+  // As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
+  // then the function returns the markup below.
+  // The tags used, the hierarchy of elements and their attributes must match the provided markup!
+  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
+  //
+  // <div class="topics">
+  //   <div class="tab">javascript</div>
+  //   <div class="tab">bootstrap</div>
+  //   <div class="tab">technology</div>
+  // </div>
+  //
+  const topicsElement = document.createElement('div');
+  topicsElement.classList.add('topics');
+
+  topics.forEach(item => {
+    const tabElem = document.createElement('div');
+    tabElem.classList.add('tab');
+    tabElem.textContent = item;
+    topicsElement.appendChild(tabElem);
+  });
+  return topicsElement;
+}
+
+const tabsAppender = (selector) => {
+  // TASK 4
+  // ---------------------
+  // Implement this function which takes a css selector as its only argument.
+  // It should obtain topics from this endpoint: `http://localhost:5001/api/topics` (test it with a console.log!).
+  // Find the array of topics inside the response, and create the tabs using the Tabs component.
+  // Append the tabs to the element in the DOM that matches the selector passed to the function.
+  //
+  axios.get(`http://localhost:5001/api/topics`)
+  .then(res => {
+    console.log(res)
+    const target = document.querySelector(selector);
+    const topic = res.data.topics;
+    const tabElem = Tabs(topic);
+    target.appendChild(tabElem);
+  })
+  .catch(err => {
+    console.error(err);
+  })
+}
+
+export { Tabs, tabsAppender }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
